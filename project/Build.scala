63e636b34898b15ebb52ae48b1f8dc28a5925de7
mjkim <mjkim@pcninc.co.kr>
2013-04-03 20:58:03 +0900

    dd

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,7 @@
 import sbt._
 import Keys._
 import play.Project._
+import com.github.play2war.plugin._
 
 object ApplicationBuild extends Build {
 
@@ -16,8 +17,12 @@ object ApplicationBuild extends Build {
 	"org.jsoup" % "jsoup" % "1.7.2"
   )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+// val main = play.Project(appName, appVersion, appDependencies).settings(
+   // Add your own project settings here      
+// )
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+  // ... Your own settings here
+    Play2WarKeys.servletVersion := "2.5"
+  ).settings(Play2WarPlugin.play2WarSettings: _*)
 
 }
